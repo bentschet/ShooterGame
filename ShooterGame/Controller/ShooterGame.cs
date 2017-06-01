534b9d916de2ffa53d63f25d231a486ad294149b
bentschet <btsc0323@740s-pl-126.local>
2017-06-01 13:46:07 -0600

    well i tried

--- ShooterGame/Controller/ShooterGame.cs
+++ ShooterGame/Controller/ShooterGame.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using ShooterGame.Controller;
+using ShooterGame.View;
+using ShooterGame.Model;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -14,6 +16,17 @@ namespace ShooterGame.Controller
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 		private Player player;
+		// Keyboard states used to determine key presses
+		private KeyboardState currentKeyboardState;
+		private KeyboardState previousKeyboardState;
+
+		// Gamepad states used to determine button presses
+		private GamePadState currentGamePadState;
+		private GamePadState previousGamePadState;
+
+		// A movement speed for the player
+		private float playerMoveSpeed;
+
 
 		public Game1()
 		{
@@ -30,8 +43,12 @@ namespace ShooterGame.Controller
 		protected override void Initialize()
 		{
 			// TODO: Add your initialization logic here
+
 			player = new Player();
+			// Set a constant player move speed
+			playerMoveSpeed = 8.0f;
 			base.Initialize();
+
 		}
 
 		/// <summary>
@@ -43,11 +60,43 @@ namespace ShooterGame.Controller
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
 
-		// Load the player resources 
-		Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			// Load the player resources
+			Animation playerAnimation = new Animation();
+			Texture2D playerTexture = Content.Load<Texture2D>("Animation/shipAnimation");
+			playerAnimation.Initialize(playerTexture, Vector2.Zero, 115, 69, 8, 30, Color.White, 1f, true);
 
-		player.Initialize(Content.Load<Texture2D>("Texture/player"), playerPosition);
-			//TODO: use this.Content to load your game content here 
+			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			player.Initialize(playerAnimation, playerPosition);
+		}
+
+		private void UpdatePlayer(GameTime gameTime)
+		{
+			player.Update(gameTime);
+			// Get Thumbstick Controls
+			player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
+
+			// Use the Keyboard / Dpad
+			if (currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
+			{
+				player.Position.X -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
+			{
+				player.Position.X += playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Up) || currentGamePadState.DPad.Up == ButtonState.Pressed)
+			{
+				player.Position.Y -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.Down) || currentGamePadState.DPad.Down == ButtonState.Pressed)
+			{
+				player.Position.Y += playerMoveSpeed;
+			}
+
+			// Make sure that the player does not go out of bounds
+			player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
 		}
 
 		/// <summary>
@@ -63,7 +112,17 @@ namespace ShooterGame.Controller
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
 #endif
+						// Save the previous state of the keyboard and game pad so we can determinesingle key/button presses
+			previousGamePadState = currentGamePadState;
+			previousKeyboardState = currentKeyboardState;
 
+			// Read the current state of the keyboard and gamepad and store it
+			currentKeyboardState = Keyboard.GetState();
+			currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+
+			//Update the player
+			UpdatePlayer(gameTime);
 			// TODO: Add your update logic here
 
 			base.Update(gameTime);
---
+++ ShooterGame/Model/Enemy.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using ShooterGame.View;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShooterGame.Model
+{
+	public class Enemy
+	{
+		public Enemy()
+		{
+		}
+
+		// Animation representing the enemy
+		public Animation enemyAnimation;
+		public Animation EnemyAnimation
+		{
+			get { return enemyAnimation; }
+			set { enemyAnimation = value; }
+		}
+
+		// The position of the enemy ship relative to the top left corner of the screen
+		public Vector2 Position;
+
+		// The state of the Enemy Ship
+		private bool active;
+		public bool Active
+		{
+			get { return active; }
+			set { active = value; }
+		}
+
+		// The hit points of the enemy, if this goes to zero the enemy dies
+		private int health;
+		public int Health
+		{
+			get { return health; }
+			set { health = value; }
+		}
+
+		// The amount of damage the enemy inflicts on the player ship
+		private int damage;
+		public int Damage
+		{
+			get { return damage; }
+			set { damage = value; }
+		}
+
+		// The amount of score the enemy will give to the player
+		private int scoreValue;
+		public int ScoreValue
+		{
+		    get { return scoreValue; } 
+		    set { scoreValue = value; } 
+		}
+
+		// Get the width of the enemy ship
+		public int Width
+		{
+			get { return EnemyAnimation.FrameWidth; }
+		}
+
+		// Get the height of the enemy ship
+		public int Height
+		{
+			get { return EnemyAnimation.FrameHeight; }
+		}
+
+		// The speed at which the enemy moves
+		float enemyMoveSpeed;
+
+		public void Initialize()
+		{
+		}
+
+		public void Update()
+		{
+		}
+
+		public void Draw()
+		{
+		}
+	}
+}
